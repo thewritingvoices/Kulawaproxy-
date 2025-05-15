52c5eafb5d5f6be09c0dc48f5e739d09bda07c08
thewritingvoices <thewritingvoices@gmail.com>
2025-05-14 17:18:52 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  OPENAI_API_KEY: "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"
+};
